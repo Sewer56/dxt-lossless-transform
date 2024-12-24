4bb26b89db86fa90beac17e02597082e4fa86cc4
Sewer56 <sewer56lol@googlemail.com>
2024-12-24 16:44:03 +0000

    Written: Contents of dxt-lossless-transform-utils

--- projects/dxt-lossless-transform-utils/src/dds/mod.rs
+++ projects/dxt-lossless-transform-utils/src/dds/mod.rs
@@ -1,45 +1,11 @@
-/// Determines if the given file represents a DDS archive.
-/// This is done by checking the 'MAGIC' header, 'DDS ' at offset 0.
-#[inline(always)]
-pub fn is_dds(ptr: *const u8, len: usize) -> bool {
-    len >= 80 && unsafe { (ptr as *const u32).read_unaligned() } == 0x20534444_u32.to_le()
-}
+/// Shared constants between modules.
+pub mod constants;
 
-#[cfg(test)]
-mod tests {
-    use core::iter::repeat;
+/// Determine if a file is a DDS file.
+pub mod is_dds;
 
-    use super::*;
+/// Extract the pixel data from a DDS file.
+pub mod parse_dds;
 
-    #[test]
-    fn test_is_dds() {
-        // Valid DDS header with sufficient length
-        let valid_data = [0x44, 0x44, 0x53, 0x20]
-            .into_iter()
-            .chain(repeat(0).take(76))
-            .collect::<Vec<u8>>();
-        assert!(is_dds(valid_data.as_ptr(), valid_data.len()));
-
-        // Valid header but insufficient length
-        let short_valid_header = [0x44, 0x44, 0x53, 0x20]
-            .into_iter()
-            .chain(repeat(0).take(75))
-            .collect::<Vec<u8>>();
-        assert!(!is_dds(
-            short_valid_header.as_ptr(),
-            short_valid_header.len()
-        ));
-
-        // Invalid header with sufficient length
-        let invalid_data = repeat(0).take(80).collect::<Vec<u8>>();
-        assert!(!is_dds(invalid_data.as_ptr(), invalid_data.len()));
-
-        // Too short
-        let short_data = [0x44, 0x44, 0x53, 0x20];
-        assert!(!is_dds(short_data.as_ptr(), short_data.len()));
-
-        // Empty data
-        let empty_data: [u8; 0] = [];
-        assert!(!is_dds(empty_data.as_ptr(), empty_data.len()));
-    }
-}
+pub use is_dds::*;
+pub use parse_dds::*;

9dc960ba7070db2e27b6dc2c2ea19f2cd937aacc
Sewer56 <sewer56lol@googlemail.com>
2024-12-25 16:43:54 +0000

    Added: Methods for transform with switch on format.

--- projects/dxt-lossless-transform-api/src/exports.rs
+++ projects/dxt-lossless-transform-api/src/exports.rs
@@ -64,6 +64,70 @@ pub unsafe extern "C" fn parse_dds(ptr: *const u8, len: usize) -> DdsInfo {
     }
 }
 
+/// Transforms data from the standard format to one that is more suitable for compression.
+///
+/// This function selects the best available implementation based on available CPU features.
+/// Hardware accelerated (SIMD) methods are currently available for x86 and x86-64.
+///
+/// # Safety
+///
+/// - `input_ptr` must be valid for reads of len bytes
+/// - `output_ptr` must be valid for writes of len bytes
+/// - `len` must be divisible by 8 (BC1 block size)
+/// - `input_ptr` and `output_ptr` must be 64-byte aligned (for performance and required by some platforms).
+/// - `format` must be a valid [`DdsFormat`]
+#[no_mangle]
+pub unsafe extern "C" fn transform_format(
+    input_ptr: *const u8,
+    output_ptr: *mut u8,
+    len: usize,
+    format: DdsFormat,
+) {
+    // Convert from C enum to internal enum
+    let internal_format = match format {
+        DdsFormat::NotADds | DdsFormat::Unknown => crate::DdsFormat::Unknown,
+        DdsFormat::BC1 => crate::DdsFormat::BC1,
+        DdsFormat::BC2 => crate::DdsFormat::BC2,
+        DdsFormat::BC3 => crate::DdsFormat::BC3,
+        DdsFormat::BC7 => crate::DdsFormat::BC7,
+    };
+
+    crate::transform_format(input_ptr, output_ptr, len, internal_format)
+}
+
+/// Untransforms data from a compression suitable one to the standard format.
+///
+/// This function selects the best available implementation based on available CPU features.
+/// Hardware accelerated (SIMD) methods are currently available for x86 and x86-64.
+///
+/// # Safety
+///
+/// - `input_ptr` must be valid for reads of len bytes
+/// - `output_ptr` must be valid for writes of len bytes
+/// - `len` must be divisible by 8 (BC1 block size)
+/// - `input_ptr` and `output_ptr` must be 64-byte aligned (for performance and required by some platforms).
+/// - `format` must be a valid [`DdsFormat`], and the same format passed to [`transform_format`].
+#[no_mangle]
+pub unsafe extern "C" fn untransform_format(
+    input_ptr: *const u8,
+    output_ptr: *mut u8,
+    len: usize,
+    format: DdsFormat,
+) {
+    // Convert from C enum to internal enum
+    let internal_format = match format {
+        DdsFormat::NotADds | DdsFormat::Unknown => {
+            dxt_lossless_transform_utils::dds::DdsFormat::Unknown
+        }
+        DdsFormat::BC1 => crate::DdsFormat::BC1,
+        DdsFormat::BC2 => crate::DdsFormat::BC2,
+        DdsFormat::BC3 => crate::DdsFormat::BC3,
+        DdsFormat::BC7 => crate::DdsFormat::BC7,
+    };
+
+    crate::untransform_format(input_ptr, output_ptr, len, internal_format)
+}
+
 /// Transform BC1 data from standard interleaved format to separated color/index format
 /// to improve compression ratio.
 ///

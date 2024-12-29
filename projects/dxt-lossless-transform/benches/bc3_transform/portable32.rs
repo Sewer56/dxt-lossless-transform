82f4b337919bc28de0cd13b95314e3e0527219b0
Sewer56 <sewer56lol@googlemail.com>
2024-12-29 19:23:22 +0000

    Added: BC3 Transform Unroll 2

--- projects/dxt-lossless-transform/benches/bc3_transform/portable32.rs
+++ projects/dxt-lossless-transform/benches/bc3_transform/portable32.rs
@@ -12,6 +12,16 @@ fn bench_portable32(b: &mut criterion::Bencher, input: &RawAlloc, output: &mut R
     });
 }
 
+fn bench_portable32_unroll_2(b: &mut criterion::Bencher, input: &RawAlloc, output: &mut RawAlloc) {
+    b.iter(|| unsafe {
+        u32_unroll_2(
+            black_box(input.as_ptr()),
+            black_box(output.as_mut_ptr()),
+            black_box(input.len()),
+        )
+    });
+}
+
 pub(crate) fn run_benchmarks(
     group: &mut criterion::BenchmarkGroup<'_, criterion::measurement::WallTime>,
     input: &RawAlloc,
@@ -25,5 +35,11 @@ pub(crate) fn run_benchmarks(
         |b, _| bench_portable32(b, input, output),
     );
 
-    if !important_benches_only {}
+    if !important_benches_only {
+        group.bench_with_input(
+            BenchmarkId::new("portable32 unroll-2", size),
+            &size,
+            |b, _| bench_portable32_unroll_2(b, input, output),
+        );
+    }
 }
--- projects/dxt-lossless-transform/src/raw/bc3/transform/portable32.rs
+++ projects/dxt-lossless-transform/src/raw/bc3/transform/portable32.rs
@@ -40,6 +40,61 @@ pub unsafe fn u32(input_ptr: *const u8, output_ptr: *mut u8, len: usize) {
     }
 }
 
+/// # Safety
+///
+/// - input_ptr must be valid for reads of len bytes
+/// - output_ptr must be valid for writes of len bytes
+/// - len must be divisible by 32 (16 * 2)
+/// - pointers must be properly aligned for u32 access
+pub unsafe fn u32_unroll_2(input_ptr: *const u8, output_ptr: *mut u8, len: usize) {
+    debug_assert!(len % 32 == 0);
+
+    let mut alpha_byte_out_ptr = output_ptr as *mut u16;
+    let mut alpha_bit_out_ptr = output_ptr.add(len / 16 * 2) as *mut u16;
+    let mut color_byte_out_ptr = output_ptr.add(len / 16 * 8) as *mut u32;
+    let mut index_byte_out_ptr = output_ptr.add(len / 16 * 12) as *mut u32;
+
+    let mut current_input_ptr = input_ptr;
+    let alpha_byte_end_ptr = alpha_bit_out_ptr;
+
+    while alpha_byte_out_ptr < alpha_byte_end_ptr {
+        // Block 1
+        let alpha_bytes1 = (current_input_ptr as *const u16).read_unaligned();
+        let alpha_bits1_a = (current_input_ptr.add(2) as *const u16).read_unaligned();
+        let alpha_bits1_b = (current_input_ptr.add(4) as *const u32).read_unaligned();
+        let color_bytes1 = (current_input_ptr.add(8) as *const u32).read_unaligned();
+        let index_bytes1 = (current_input_ptr.add(12) as *const u32).read_unaligned();
+
+        // Block 2
+        let alpha_bytes2 = (current_input_ptr.add(16) as *const u16).read_unaligned();
+        let alpha_bits2_a = (current_input_ptr.add(18) as *const u16).read_unaligned();
+        let alpha_bits2_b = (current_input_ptr.add(20) as *const u32).read_unaligned();
+        let color_bytes2 = (current_input_ptr.add(24) as *const u32).read_unaligned();
+        let index_bytes2 = (current_input_ptr.add(28) as *const u32).read_unaligned();
+
+        // Write Block 1
+        alpha_byte_out_ptr.write_unaligned(alpha_bytes1);
+        alpha_bit_out_ptr.write_unaligned(alpha_bits1_a);
+        (alpha_bit_out_ptr.add(1) as *mut u32).write_unaligned(alpha_bits1_b);
+        color_byte_out_ptr.write_unaligned(color_bytes1);
+        index_byte_out_ptr.write_unaligned(index_bytes1);
+
+        // Write Block 2
+        alpha_byte_out_ptr.add(1).write_unaligned(alpha_bytes2);
+        alpha_bit_out_ptr.add(3).write_unaligned(alpha_bits2_a);
+        (alpha_bit_out_ptr.add(4) as *mut u32).write_unaligned(alpha_bits2_b);
+        color_byte_out_ptr.add(1).write_unaligned(color_bytes2);
+        index_byte_out_ptr.add(1).write_unaligned(index_bytes2);
+
+        // Advance pointers
+        alpha_byte_out_ptr = alpha_byte_out_ptr.add(2);
+        alpha_bit_out_ptr = alpha_bit_out_ptr.add(6);
+        color_byte_out_ptr = color_byte_out_ptr.add(2);
+        index_byte_out_ptr = index_byte_out_ptr.add(2);
+        current_input_ptr = current_input_ptr.add(32);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,6 +116,12 @@ mod tests {
         name: "u32 no-unroll",
         func: u32,
         min_blocks: 1, // 16 bytes, minimum size
+        many_blocks: 8,
+    })]
+    #[case::u32_unroll_2(TestCase {
+        name: "u32 unroll-2",
+        func: u32_unroll_2,
+        min_blocks: 2, // 32 bytes minimum
         many_blocks: 8,
     })]
     fn test_transform(#[case] test_case: TestCase) {

28ebc400c13292c6bb5a34e328a847485184e4a1
Sewer56 <sewer56lol@googlemail.com>
2025-03-14 21:21:56 +0000

    Docs: Fix crate module readme references, now points to proper crate readme.

--- projects/dxt-lossless-transform-bc3/src/lib.rs
+++ projects/dxt-lossless-transform-bc3/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../../README.MD")]
+#![doc = include_str!(concat!("../", std::env!("CARGO_PKG_README")))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod split_blocks;

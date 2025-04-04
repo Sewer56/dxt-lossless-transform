793c2904a17ffb4512377c7c87316d89d637df3b
Sewer56 <sewer56lol@googlemail.com>
2025-04-04 16:19:35 +0100

    BC3: Introduce u32_with_separate_endpoints to split options

--- projects/dxt-lossless-transform-bc3/src/split_blocks/split/portable32.rs
+++ projects/dxt-lossless-transform-bc3/src/split_blocks/split/portable32.rs
@@ -7,11 +7,33 @@
 pub unsafe fn u32(input_ptr: *const u8, output_ptr: *mut u8, len: usize) {
     debug_assert!(len % 16 == 0);
 
-    let mut alpha_byte_out_ptr = output_ptr as *mut u16;
-    let mut alpha_bit_out_ptr = output_ptr.add(len / 16 * 2) as *mut u16;
-    let mut color_byte_out_ptr = output_ptr.add(len / 16 * 8) as *mut u32;
-    let mut index_byte_out_ptr = output_ptr.add(len / 16 * 12) as *mut u32;
+    let alpha_byte_out_ptr = output_ptr as *mut u16;
+    let alpha_bit_out_ptr = output_ptr.add(len / 16 * 2) as *mut u16;
+    let color_byte_out_ptr = output_ptr.add(len / 16 * 8) as *mut u32;
+    let index_byte_out_ptr = output_ptr.add(len / 16 * 12) as *mut u32;
 
+    u32_with_separate_endpoints(
+        input_ptr,
+        alpha_byte_out_ptr,
+        alpha_bit_out_ptr,
+        color_byte_out_ptr,
+        index_byte_out_ptr,
+    );
+}
+
+/// # Safety
+///
+/// - input_ptr must be valid for reads of len bytes
+/// - output_ptr must be valid for writes of len bytes
+/// - len must be divisible by 16
+/// - pointers must be properly aligned for u32 access
+pub unsafe fn u32_with_separate_endpoints(
+    input_ptr: *const u8,
+    mut alpha_byte_out_ptr: *mut u16,
+    mut alpha_bit_out_ptr: *mut u16,
+    mut color_byte_out_ptr: *mut u32,
+    mut index_byte_out_ptr: *mut u32,
+) {
     let mut current_input_ptr = input_ptr;
     let alpha_byte_end_ptr = alpha_bit_out_ptr;
 

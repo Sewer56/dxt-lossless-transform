8c31c58f2fdac0aae238606f6010f5ae9988f9b2
Sewer56 <sewer56lol@googlemail.com>
2025-04-04 09:29:05 +0100

    BC1: Make SSE2 Unsplit accept any length

--- projects/dxt-lossless-transform-bc1/src/split_blocks/unsplit/sse2.rs
+++ projects/dxt-lossless-transform-bc1/src/split_blocks/unsplit/sse2.rs
@@ -1,3 +1,4 @@
+use crate::split_blocks::unsplit::portable32::u32_detransform_with_separate_pointers;
 use std::arch::asm;
 
 /// # Safety
@@ -5,46 +6,59 @@ use std::arch::asm;
 /// - input_ptr must be valid for reads of len bytes
 /// - output_ptr must be valid for writes of len bytes
 /// - pointers must be properly aligned for SSE operations
-/// - len must be divisible by 32
 #[allow(unused_assignments)]
 #[target_feature(enable = "sse2")]
 pub unsafe fn unpck_detransform(mut input_ptr: *const u8, mut output_ptr: *mut u8, len: usize) {
-    debug_assert!(len % 32 == 0);
+    // Process as many 32-byte blocks as possible
+    let aligned_len = len - (len % 32);
+    let mut indices_ptr = input_ptr.add(len / 2);
+    let mut colors_aligned_end = input_ptr.add(aligned_len / 2);
 
-    unsafe {
-        let mut indices_ptr = input_ptr.add(len / 2);
-        let mut end = input_ptr.add(len);
-        asm!(
-            ".p2align 5",
-            "2:",
-            // Load colors and indices (16 bytes each)
-            "movdqu {xmm0}, [{colors_ptr}]",    // colors
-            "add {colors_ptr}, 16",
-            "movdqu {xmm1}, [{indices_ptr}]",   // indices
-            "add {indices_ptr}, 16",
+    if aligned_len > 0 {
+        unsafe {
+            asm!(
+                ".p2align 5",
+                "2:",
+                // Load colors and indices (16 bytes each)
+                "movdqu {xmm0}, [{colors_ptr}]",    // colors
+                "add {colors_ptr}, 16",
+                "movdqu {xmm1}, [{indices_ptr}]",   // indices
+                "add {indices_ptr}, 16",
 
-            // Interleave the 32-bit values
-            "movaps {xmm2}, {xmm0}",
-            "unpcklps {xmm0}, {xmm1}",    // Low half: color0,index0,color1,index1
-            "unpckhps {xmm2}, {xmm1}",    // High half: color2,index2,color3,index3
+                // Interleave the 32-bit values
+                "movaps {xmm2}, {xmm0}",
+                "unpcklps {xmm0}, {xmm1}",    // Low half: color0,index0,color1,index1
+                "unpckhps {xmm2}, {xmm1}",    // High half: color2,index2,color3,index3
 
-            // Store the results
-            "movdqu [{dst_ptr}], {xmm0}",
-            "movdqu [{dst_ptr} + 16], {xmm2}",
-            "add {dst_ptr}, 32",
+                // Store the results
+                "movdqu [{dst_ptr}], {xmm0}",
+                "movdqu [{dst_ptr} + 16], {xmm2}",
+                "add {dst_ptr}, 32",
 
-            // Continue if we haven't reached the end
-            "cmp {indices_ptr}, {end}",
-            "jb 2b",
+                // Continue if we haven't reached the end
+                "cmp {colors_ptr}, {end}",
+                "jb 2b",
 
-            colors_ptr = inout(reg) input_ptr,
-            indices_ptr = inout(reg) indices_ptr,
-            dst_ptr = inout(reg) output_ptr,
-            end = inout(reg) end,
-            xmm0 = out(xmm_reg) _,
-            xmm1 = out(xmm_reg) _,
-            xmm2 = out(xmm_reg) _,
-            options(nostack)
+                colors_ptr = inout(reg) input_ptr,
+                indices_ptr = inout(reg) indices_ptr,
+                dst_ptr = inout(reg) output_ptr,
+                end = inout(reg) colors_aligned_end,
+                xmm0 = out(xmm_reg) _,
+                xmm1 = out(xmm_reg) _,
+                xmm2 = out(xmm_reg) _,
+                options(nostack)
+            );
+        }
+    }
+
+    // Process any remaining elements after the aligned blocks
+    let remaining = len - aligned_len;
+    if remaining > 0 {
+        u32_detransform_with_separate_pointers(
+            input_ptr as *const u32,
+            indices_ptr as *const u32,
+            output_ptr,
+            remaining,
         );
     }
 }
@@ -54,7 +68,6 @@ pub unsafe fn unpck_detransform(mut input_ptr: *const u8, mut output_ptr: *mut u
 /// - input_ptr must be valid for reads of len bytes
 /// - output_ptr must be valid for writes of len bytes
 /// - pointers must be properly aligned for SSE operations
-/// - len must be divisible by 64 (processes 2 blocks per iteration)
 #[allow(unused_assignments)]
 #[target_feature(enable = "sse2")]
 pub unsafe fn unpck_detransform_unroll_2(
@@ -62,54 +75,68 @@ pub unsafe fn unpck_detransform_unroll_2(
     mut output_ptr: *mut u8,
     len: usize,
 ) {
-    debug_assert!(len % 64 == 0);
+    // Process as many 64-byte blocks as possible
+    let aligned_len = len - (len % 64);
+    let mut indices_ptr = input_ptr.add(len / 2);
+    let mut colors_aligned_end = input_ptr.add(aligned_len / 2);
 
-    unsafe {
-        let mut indices_ptr = input_ptr.add(len / 2);
-        let mut end = input_ptr.add(len);
-        asm!(
-            ".p2align 5",
-            "2:",
-            // Load all colors and indices (32 bytes each)
-            "movdqu {xmm0}, [{colors_ptr}]",      // colors 0
-            "movdqu {xmm3}, [{colors_ptr} + 16]", // colors 1
-            "add {colors_ptr}, 32",
-            "movdqu {xmm1}, [{indices_ptr}]",     // indices 0
-            "movdqu {xmm4}, [{indices_ptr} + 16]", // indices 1
-            "add {indices_ptr}, 32",
+    if aligned_len > 0 {
+        unsafe {
+            asm!(
+                ".p2align 5",
+                "2:",
+                // Load all colors and indices (32 bytes each)
+                "movdqu {xmm0}, [{colors_ptr}]",      // colors 0
+                "movdqu {xmm3}, [{colors_ptr} + 16]", // colors 1
+                "add {colors_ptr}, 32",
+                "movdqu {xmm1}, [{indices_ptr}]",     // indices 0
+                "movdqu {xmm4}, [{indices_ptr} + 16]", // indices 1
+                "add {indices_ptr}, 32",
 
-            // Save copies for high parts
-            "movaps {xmm2}, {xmm0}", // colors 0 copy
-            "movaps {xmm5}, {xmm3}", // colors 1 copy
+                // Save copies for high parts
+                "movaps {xmm2}, {xmm0}", // colors 0 copy
+                "movaps {xmm5}, {xmm3}", // colors 1 copy
 
-            // Unpack all blocks
-            "punpckldq {xmm0}, {xmm1}", // color0,index0,color1,index1
-            "punpckldq {xmm3}, {xmm4}", // color4,index4,color5,index5
-            "punpckhdq {xmm2}, {xmm1}", // color2,index2,color3,index3
-            "punpckhdq {xmm5}, {xmm4}", // color6,index6,color7,index7
+                // Unpack all blocks
+                "punpckldq {xmm0}, {xmm1}", // color0,index0,color1,index1
+                "punpckldq {xmm3}, {xmm4}", // color4,index4,color5,index5
+                "punpckhdq {xmm2}, {xmm1}", // color2,index2,color3,index3
+                "punpckhdq {xmm5}, {xmm4}", // color6,index6,color7,index7
 
-            // Store all results
-            "movdqu [{dst_ptr}], {xmm0}",
-            "movdqu [{dst_ptr} + 16], {xmm2}",
-            "movdqu [{dst_ptr} + 32], {xmm3}",
-            "movdqu [{dst_ptr} + 48], {xmm5}",
-            "add {dst_ptr}, 64",
+                // Store all results
+                "movdqu [{dst_ptr}], {xmm0}",
+                "movdqu [{dst_ptr} + 16], {xmm2}",
+                "movdqu [{dst_ptr} + 32], {xmm3}",
+                "movdqu [{dst_ptr} + 48], {xmm5}",
+                "add {dst_ptr}, 64",
 
-            // Continue if we haven't reached the end
-            "cmp {indices_ptr}, {end}",
-            "jb 2b",
+                // Continue if we haven't reached the end
+                "cmp {colors_ptr}, {end}",
+                "jb 2b",
 
-            colors_ptr = inout(reg) input_ptr,
-            indices_ptr = inout(reg) indices_ptr,
-            dst_ptr = inout(reg) output_ptr,
-            end = inout(reg) end,
-            xmm0 = out(xmm_reg) _,
-            xmm1 = out(xmm_reg) _,
-            xmm2 = out(xmm_reg) _,
-            xmm3 = out(xmm_reg) _,
-            xmm4 = out(xmm_reg) _,
-            xmm5 = out(xmm_reg) _,
-            options(nostack)
+                colors_ptr = inout(reg) input_ptr,
+                indices_ptr = inout(reg) indices_ptr,
+                dst_ptr = inout(reg) output_ptr,
+                end = inout(reg) colors_aligned_end,
+                xmm0 = out(xmm_reg) _,
+                xmm1 = out(xmm_reg) _,
+                xmm2 = out(xmm_reg) _,
+                xmm3 = out(xmm_reg) _,
+                xmm4 = out(xmm_reg) _,
+                xmm5 = out(xmm_reg) _,
+                options(nostack)
+            );
+        }
+    }
+
+    // Process any remaining elements after the aligned blocks
+    let remaining = len - aligned_len;
+    if remaining > 0 {
+        u32_detransform_with_separate_pointers(
+            input_ptr as *const u32,
+            indices_ptr as *const u32,
+            output_ptr,
+            remaining,
         );
     }
 }
@@ -119,7 +146,6 @@ pub unsafe fn unpck_detransform_unroll_2(
 /// - input_ptr must be valid for reads of len bytes
 /// - output_ptr must be valid for writes of len bytes
 /// - pointers must be properly aligned for SSE operations
-/// - len must be divisible by 128 (processes 4 blocks per iteration)
 #[cfg(target_arch = "x86_64")]
 #[allow(unused_assignments)]
 #[target_feature(enable = "sse2")]
@@ -128,76 +154,90 @@ pub unsafe fn unpck_detransform_unroll_4(
     mut output_ptr: *mut u8,
     len: usize,
 ) {
-    debug_assert!(len % 128 == 0);
+    // Process as many 128-byte blocks as possible
+    let aligned_len = len - (len % 128);
+    let mut indices_ptr = input_ptr.add(len / 2);
+    let mut colors_aligned_end = input_ptr.add(aligned_len / 2);
 
-    unsafe {
-        let mut indices_ptr = input_ptr.add(len / 2);
-        let mut end = input_ptr.add(len);
-        asm!(
-            ".p2align 5",
-            "2:",
-            // Load all colors (64 bytes)
-            "movdqu {xmm0}, [{colors_ptr}]",      // colors 0
-            "movdqu {xmm3}, [{colors_ptr} + 16]", // colors 1
-            "movdqu {xmm6}, [{colors_ptr} + 32]", // colors 2
-            "movdqu {xmm9}, [{colors_ptr} + 48]", // colors 3
-            "add {colors_ptr}, 64",
+    if aligned_len > 0 {
+        unsafe {
+            asm!(
+                ".p2align 5",
+                "2:",
+                // Load all colors (64 bytes)
+                "movdqu {xmm0}, [{colors_ptr}]",      // colors 0
+                "movdqu {xmm3}, [{colors_ptr} + 16]", // colors 1
+                "movdqu {xmm6}, [{colors_ptr} + 32]", // colors 2
+                "movdqu {xmm9}, [{colors_ptr} + 48]", // colors 3
+                "add {colors_ptr}, 64",
 
-            // Load all indices (64 bytes)
-            "movdqu {xmm1}, [{indices_ptr}]",      // indices 0
-            "movdqu {xmm4}, [{indices_ptr} + 16]", // indices 1
-            "movdqu {xmm7}, [{indices_ptr} + 32]", // indices 2
-            "movdqu {xmm10}, [{indices_ptr} + 48]", // indices 3
-            "add {indices_ptr}, 64",
+                // Load all indices (64 bytes)
+                "movdqu {xmm1}, [{indices_ptr}]",      // indices 0
+                "movdqu {xmm4}, [{indices_ptr} + 16]", // indices 1
+                "movdqu {xmm7}, [{indices_ptr} + 32]", // indices 2
+                "movdqu {xmm10}, [{indices_ptr} + 48]", // indices 3
+                "add {indices_ptr}, 64",
 
-            // Save copies for high parts
-            "movaps {xmm2}, {xmm0}",  // colors 0 copy
-            "movaps {xmm5}, {xmm3}",  // colors 1 copy
-            "movaps {xmm8}, {xmm6}",  // colors 2 copy
-            "movaps {xmm11}, {xmm9}", // colors 3 copy
+                // Save copies for high parts
+                "movaps {xmm2}, {xmm0}",  // colors 0 copy
+                "movaps {xmm5}, {xmm3}",  // colors 1 copy
+                "movaps {xmm8}, {xmm6}",  // colors 2 copy
+                "movaps {xmm11}, {xmm9}", // colors 3 copy
 
-            // Unpack all blocks
-            "unpcklps {xmm0}, {xmm1}",
-            "unpckhps {xmm2}, {xmm1}",
-            "unpcklps {xmm3}, {xmm4}",
-            "unpckhps {xmm5}, {xmm4}",
-            "unpcklps {xmm6}, {xmm7}",
-            "unpckhps {xmm8}, {xmm7}",
-            "unpcklps {xmm9}, {xmm10}",
-            "unpckhps {xmm11}, {xmm10}",
+                // Unpack all blocks
+                "unpcklps {xmm0}, {xmm1}",
+                "unpckhps {xmm2}, {xmm1}",
+                "unpcklps {xmm3}, {xmm4}",
+                "unpckhps {xmm5}, {xmm4}",
+                "unpcklps {xmm6}, {xmm7}",
+                "unpckhps {xmm8}, {xmm7}",
+                "unpcklps {xmm9}, {xmm10}",
+                "unpckhps {xmm11}, {xmm10}",
 
-            // Store all results
-            "movdqu [{dst_ptr}], {xmm0}",
-            "movdqu [{dst_ptr} + 16], {xmm2}",
-            "movdqu [{dst_ptr} + 32], {xmm3}",
-            "movdqu [{dst_ptr} + 48], {xmm5}",
-            "movdqu [{dst_ptr} + 64], {xmm6}",
-            "movdqu [{dst_ptr} + 80], {xmm8}",
-            "movdqu [{dst_ptr} + 96], {xmm9}",
-            "movdqu [{dst_ptr} + 112], {xmm11}",
-            "add {dst_ptr}, 128",
+                // Store all results
+                "movdqu [{dst_ptr}], {xmm0}",
+                "movdqu [{dst_ptr} + 16], {xmm2}",
+                "movdqu [{dst_ptr} + 32], {xmm3}",
+                "movdqu [{dst_ptr} + 48], {xmm5}",
+                "movdqu [{dst_ptr} + 64], {xmm6}",
+                "movdqu [{dst_ptr} + 80], {xmm8}",
+                "movdqu [{dst_ptr} + 96], {xmm9}",
+                "movdqu [{dst_ptr} + 112], {xmm11}",
+                "add {dst_ptr}, 128",
 
-            // Continue if we haven't reached the end
-            "cmp {indices_ptr}, {end}",
-            "jb 2b",
+                // Continue if we haven't reached the end
+                "cmp {colors_ptr}, {end}",
+                "jb 2b",
 
-            colors_ptr = inout(reg) input_ptr,
-            indices_ptr = inout(reg) indices_ptr,
-            dst_ptr = inout(reg) output_ptr,
-            end = inout(reg) end,
-            xmm0 = out(xmm_reg) _,
-            xmm1 = out(xmm_reg) _,
-            xmm2 = out(xmm_reg) _,
-            xmm3 = out(xmm_reg) _,
-            xmm4 = out(xmm_reg) _,
-            xmm5 = out(xmm_reg) _,
-            xmm6 = out(xmm_reg) _,
-            xmm7 = out(xmm_reg) _,
-            xmm8 = out(xmm_reg) _,
-            xmm9 = out(xmm_reg) _,
-            xmm10 = out(xmm_reg) _,
-            xmm11 = out(xmm_reg) _,
-            options(nostack)
+                colors_ptr = inout(reg) input_ptr,
+                indices_ptr = inout(reg) indices_ptr,
+                dst_ptr = inout(reg) output_ptr,
+                end = inout(reg) colors_aligned_end,
+                xmm0 = out(xmm_reg) _,
+                xmm1 = out(xmm_reg) _,
+                xmm2 = out(xmm_reg) _,
+                xmm3 = out(xmm_reg) _,
+                xmm4 = out(xmm_reg) _,
+                xmm5 = out(xmm_reg) _,
+                xmm6 = out(xmm_reg) _,
+                xmm7 = out(xmm_reg) _,
+                xmm8 = out(xmm_reg) _,
+                xmm9 = out(xmm_reg) _,
+                xmm10 = out(xmm_reg) _,
+                xmm11 = out(xmm_reg) _,
+                options(nostack)
+            );
+        }
+    }
+
+    // Process any remaining elements after the aligned blocks
+    let remaining = len - aligned_len;
+    if remaining > 0 {
+        u32_detransform_with_separate_pointers(
+            input_ptr as *const u32,
+            indices_ptr as *const u32,
+            output_ptr,
+            remaining,
         );
     }
 }
@@ -214,35 +254,29 @@ mod tests {
     struct TestCase {
         name: &'static str,
         func: DetransformFn,
-        min_blocks: usize,
-        many_blocks: usize,
+        block_sizes: Vec<usize>,
     }
 
     #[rstest]
     #[case::unpck(TestCase {
         name: "unpck",
         func: unpck_detransform,
-        min_blocks: 4,
-        many_blocks: 1024,
+        block_sizes: vec![1, 3, 4, 5, 7, 8, 16, 1024],
     })]
     #[case::unpck_unroll_2(TestCase {
         name: "unpck_unroll_2",
         func: unpck_detransform_unroll_2,
-        min_blocks: 8,  // Needs at least 8 blocks due to 64-byte alignment requirement
-        many_blocks: 1024,
+        block_sizes: vec![1, 3, 4, 7, 8, 9, 15, 16, 1024],
     })]
     #[cfg_attr(target_arch = "x86_64", case::unpck_unroll_4(TestCase {
         name: "unpck_unroll_4",
         func: unpck_detransform_unroll_4,
-        min_blocks: 16, // Needs at least 16 blocks due to 128-byte alignment requirement
-        many_blocks: 1024,
+        block_sizes: vec![1, 3, 4, 8, 15, 16, 17, 31, 32, 1024],
     }))]
     fn test_detransform(#[case] test_case: TestCase) {
-        // Test with minimum blocks
-        test_blocks(&test_case, test_case.min_blocks);
-
-        // Test with many blocks
-        test_blocks(&test_case, test_case.many_blocks);
+        for &num_blocks in &test_case.block_sizes {
+            test_blocks(&test_case, num_blocks);
+        }
     }
 
     fn test_blocks(test_case: &TestCase, num_blocks: usize) {

695eee2f978fdf784b969e1375b45ae732f0e6bc
Sewer56 <sewer56lol@googlemail.com>
2025-04-11 18:21:07 +0100

    Added: u32_with_separate_endpoints for split_color_endpoints

--- projects/dxt-lossless-transform-common/src/transforms/split_color_endpoints/portable32.rs
+++ projects/dxt-lossless-transform-common/src/transforms/split_color_endpoints/portable32.rs
@@ -23,10 +23,35 @@ pub unsafe fn u32(colors: *const u8, colors_out: *mut u8, colors_len_bytes: usiz
 
     // Cast input/output to u32 pointers for direct value access
     let max_input_ptr = colors.add(colors_len_bytes) as *const u32;
-    let mut input = colors as *const u32;
-    let mut output0 = colors_out as *mut u16;
-    let mut output1 = colors_out.add(colors_len_bytes / size_of::<Color565>()) as *mut u16;
+    let input = colors as *const u32;
+    let output0 = colors_out as *mut u16;
+    let output1 = colors_out.add(colors_len_bytes / size_of::<Color565>()) as *mut u16;
 
+    u32_with_separate_endpoints(max_input_ptr, input, output0, output1);
+}
+
+/// Splits the colour endpoints using 32-bit operations with separate endpoints
+///
+/// # Arguments
+///
+/// * `max_input_ptr` - Pointer to the end of the input array
+/// * `input` - Pointer to the input array of colors
+/// * `output0` - Pointer to the output array of colors (first endpoints)
+/// * `output1` - Pointer to the output array of colors (second endpoints)
+///
+/// # Safety
+///
+/// * `input` must be a valid pointer to an array of RGB565 endpoints
+/// * `max_input_ptr` must be a valid pointer to the end of the input array (not before input)
+/// * `output0` and `output1` must both be valid pointers to arrays of u16 endpoints
+/// * All arrays must have sufficient capacity for the operation to complete
+#[inline(always)]
+pub unsafe fn u32_with_separate_endpoints(
+    max_input_ptr: *const u32,
+    mut input: *const u32,
+    mut output0: *mut u16,
+    mut output1: *mut u16,
+) {
     while input < max_input_ptr {
         // Read color0 and color1 (interleaved in input)
         let color0 = input.read_unaligned();

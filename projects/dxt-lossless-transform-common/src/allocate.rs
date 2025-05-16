20a3ca28e07a8dc10b505204c865faf0d615f643
Sewer56 <sewer56lol@googlemail.com>
2025-05-16 23:55:39 +0100

    Added: Ability to create an allocation of RawArray via helper.

--- projects/dxt-lossless-transform-common/src/allocate.rs
+++ projects/dxt-lossless-transform-common/src/allocate.rs
@@ -7,26 +7,49 @@
 //! etc.
 
 use core::alloc::{Layout, LayoutError};
-
+use core::mem::MaybeUninit;
 use safe_allocator_api::allocator_api::*;
 use safe_allocator_api::RawAlloc;
 use thiserror::Error;
 
-/*
-/// Allocates an array of function pointers.
-///
-/// # Parameters
-///
-/// - `num_bytes`: The number of bytes to allocate in each element
-///
-/// # Returns
-///
-/// A [`RawAlloc`] containing the allocated data
-pub fn allocate_array<T>(num_bytes: usize) -> Result<[*mut u8; num_bytes], AllocateError> {
-    let layout = Layout::from_size_align(num_bytes, 64)?;
-    RawAlloc::new(layout)?
+/// Represents a fixed-size array of [`RawAlloc`]s.
+pub struct FixedRawAllocArray<const NUM_ELEMENTS: usize> {
+    /// The underlying raw allocations.
+    pub allocations: [RawAlloc; NUM_ELEMENTS],
 }
-    */
+
+impl<const NUM_ELEMENTS: usize> FixedRawAllocArray<NUM_ELEMENTS> {
+    /// Allocates a [`FixedRawAllocArray`] holding an inner array of [`RawAlloc`]s.
+    ///
+    /// # Parameters
+    ///
+    /// - `num_bytes`: The number of bytes to allocate in each element
+    ///
+    /// # Returns
+    ///
+    /// A [`FixedRawAllocArray`] containing the allocated data
+    #[inline]
+    pub fn new(num_bytes: usize) -> Result<Self, AllocateError> {
+        #[allow(clippy::uninit_assumed_init)]
+        // RawAlloc has no default, and we initialize all elements below
+        let mut allocations =
+            unsafe { MaybeUninit::<[RawAlloc; NUM_ELEMENTS]>::uninit().assume_init() };
+        for item in allocations.iter_mut().take(NUM_ELEMENTS) {
+            *item = allocate_align_64(num_bytes)?;
+        }
+        Ok(Self { allocations })
+    }
+
+    /// Gets a slice of just the pointers to the start of each allocation.
+    ///
+    /// # Returns
+    ///
+    /// A slice of [`RawAlloc`]s
+    #[inline]
+    pub fn get_pointer_slice(&mut self) -> [*mut u8; NUM_ELEMENTS] {
+        core::array::from_fn(|x| self.allocations[x].as_mut_ptr())
+    }
+}
 
 /// Allocates data with an alignment of 64 bytes.
 ///

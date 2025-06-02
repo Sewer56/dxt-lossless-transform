2919390e92af165a7a5e65f9d13408fb44d88872
Sewer56 <sewer56lol@googlemail.com>
2025-06-02 12:29:27 +0100

    Removed: Legacy 'multiversion' attributes for double split blocks.

--- projects/dxt-lossless-transform-bc1/src/detransform/unsplit_split_colour_split_blocks/mod.rs
+++ projects/dxt-lossless-transform-bc1/src/detransform/unsplit_split_colour_split_blocks/mod.rs
@@ -97,27 +97,6 @@ pub(crate) unsafe fn unsplit_split_colour_split_blocks(
     }
 }
 
-#[cfg_attr(
-    not(feature = "nightly"), 
-    multiversion(targets(
-        // avx512 only in nightly.
-        // x86-64-v3 without lahfsahf
-        "x86_64+avx+avx2+bmi1+bmi2+cmpxchg16b+f16c+fma+fxsr+lzcnt+movbe+popcnt+sse+sse2+sse3+sse4.1+sse4.2+ssse3+xsave",
-        // x86-64-v2 without lahfsahf
-        "x86_64+cmpxchg16b+fxsr+popcnt+sse+sse2+sse3+sse4.1+sse4.2+ssse3",
-    ))
-)]
-#[cfg_attr(
-    feature = "nightly",
-    multiversion(targets(
-        // x86-64-v4 without lahfsahf
-        "x86_64+avx+avx2+avx512bw+avx512cd+avx512dq+avx512f+avx512vl+bmi1+bmi2+cmpxchg16b+f16c+fma+fxsr+lzcnt+movbe+popcnt+sse+sse2+sse3+sse4.1+sse4.2+ssse3+xsave",
-        // x86-64-v3 without lahfsahf
-        "x86_64+avx+avx2+bmi1+bmi2+cmpxchg16b+f16c+fma+fxsr+lzcnt+movbe+popcnt+sse+sse2+sse3+sse4.1+sse4.2+ssse3+xsave",
-        // x86-64-v2 without lahfsahf
-        "x86_64+cmpxchg16b+fxsr+popcnt+sse+sse2+sse3+sse4.1+sse4.2+ssse3",
-    ))
-)]
 pub(crate) unsafe fn unsplit_split_colour_split_blocks_generic(
     color0_ptr: *const u16,
     color1_ptr: *const u16,

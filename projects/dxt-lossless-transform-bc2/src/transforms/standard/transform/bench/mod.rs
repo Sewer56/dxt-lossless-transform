4b5013cfa88985bd77eaae9afaa69dd6130069a8
Sewer56 <sewer56lol@googlemail.com>
2025-06-15 23:23:24 +0100

    Fixed: i686 build due to missing x86 exclusion

--- projects/dxt-lossless-transform-bc2/src/transforms/standard/transform/bench/mod.rs
+++ projects/dxt-lossless-transform-bc2/src/transforms/standard/transform/bench/mod.rs
@@ -19,7 +19,7 @@ pub mod portable32;
 pub use portable32::*;
 
 // Wrapper functions for benchmarks
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+#[cfg(target_arch = "x86_64")]
 pub unsafe fn sse2_shuffle_v3(input_ptr: *const u8, output_ptr: *mut u8, len: usize) {
     super::sse2::shuffle_v3(input_ptr, output_ptr, len)
 }

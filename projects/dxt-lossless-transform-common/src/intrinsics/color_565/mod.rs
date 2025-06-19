a396078dbba8600e58b65121101cc1a1a36684d6
Sewer56 <sewer56lol@googlemail.com>
2025-06-19 19:49:32 +0100

    Fixed: Broken link to Color565

--- projects/dxt-lossless-transform-common/src/intrinsics/color_565/mod.rs
+++ projects/dxt-lossless-transform-common/src/intrinsics/color_565/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! This module provides intrinsic functions for performing color-related operations
 //! directly on SIMD registers containing [`Color565`] data.
+//!
+//! [`Color565`]: crate::color_565::Color565
 
 pub mod decorrelate;
 pub mod recorrelate;

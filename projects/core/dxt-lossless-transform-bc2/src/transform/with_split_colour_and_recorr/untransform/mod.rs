85eaddb8212cdd109f3114803d192f959ffa8587
Sewer56 <sewer56lol@googlemail.com>
2025-12-13 15:44:39 +0000

    Renamed: avx512.rs -> avx512bw.rs for bc2 with_split_colour_and_recorr untransform

--- projects/core/dxt-lossless-transform-bc2/src/transform/with_split_colour_and_recorr/untransform/mod.rs
+++ projects/core/dxt-lossless-transform-bc2/src/transform/with_split_colour_and_recorr/untransform/mod.rs
@@ -6,7 +6,7 @@ use dxt_lossless_transform_common::color_565::YCoCgVariant;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod avx2;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-mod avx512;
+mod avx512bw;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod sse2;
 
@@ -79,7 +79,7 @@ unsafe fn untransform_with_split_colour_and_recorr_x86(
     #[cfg(not(feature = "no-runtime-cpu-detection"))]
     {
         if has_avx512bw() {
-            avx512::untransform_with_split_colour_and_recorr(
+            avx512bw::untransform_with_split_colour_and_recorr(
                 alpha_ptr,
                 color0_ptr,
                 color1_ptr,
@@ -121,7 +121,7 @@ unsafe fn untransform_with_split_colour_and_recorr_x86(
     #[cfg(feature = "no-runtime-cpu-detection")]
     {
         if cfg!(target_feature = "avx512bw") {
-            avx512::untransform_with_split_colour_and_recorr(
+            avx512bw::untransform_with_split_colour_and_recorr(
                 alpha_ptr,
                 color0_ptr,
                 color1_ptr,

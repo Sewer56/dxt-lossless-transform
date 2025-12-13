f2bae7d552892bb8bd38372216aef91771fad221
Sewer56 <sewer56lol@googlemail.com>
2025-12-13 14:58:42 +0000

    Renamed with_split_colour_and_recorr avx512 -> avx512bw

--- projects/core/dxt-lossless-transform-bc1/src/transform/with_split_colour_and_recorr/transform/mod.rs
+++ projects/core/dxt-lossless-transform-bc1/src/transform/with_split_colour_and_recorr/transform/mod.rs
@@ -15,7 +15,7 @@ mod generic;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod avx2;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-mod avx512;
+mod avx512bw;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod sse2;
 
@@ -81,7 +81,7 @@ unsafe fn transform_with_split_colour_and_recorr_x86(
     #[cfg(not(feature = "no-runtime-cpu-detection"))]
     {
         if has_avx512bw() {
-            avx512::transform_with_split_colour_and_decorr(
+            avx512bw::transform_with_split_colour_and_decorr(
                 input_ptr,
                 color0_ptr,
                 color1_ptr,
@@ -119,7 +119,7 @@ unsafe fn transform_with_split_colour_and_recorr_x86(
     #[cfg(feature = "no-runtime-cpu-detection")]
     {
         if cfg!(target_feature = "avx512bw") {
-            avx512::transform_with_split_colour_and_decorr(
+            avx512bw::transform_with_split_colour_and_decorr(
                 input_ptr,
                 color0_ptr,
                 color1_ptr,
